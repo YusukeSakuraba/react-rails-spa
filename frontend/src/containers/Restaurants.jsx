efcea47d7835ba6bc353ffd6a7f69c854452ac2e
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-06-01 07:11:12 +0900

    [feat]apiのデータを画面に表示

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,7 +1,14 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useReducer, useEffect } from 'react';
 import styled from 'styled-components';
 
 import { fetchRestaurants } from '../apis/restaurants';
+
+// reducers
+import {
+  initialState,
+  restaurantsActionTyps,
+  restaurantsReducer,
+} from '../reducers/restaurants';
 
 import MainLogo from '../images/logo.png';
 import MainCoverImage from '../images/main-cover-image.png';
@@ -26,10 +33,18 @@ const MainCover = styled.img`
 
 
 export const Restaurants = () => {
+
+  const [state, dispatch] = useReducer(restaurantsReducer, initialState);
   useEffect(() => {
+    dispatch({ type: restaurantsActionTyps.FETCHING });
     fetchRestaurants()
       .then((data) =>
-        console.log(data)
+        dispatch({
+          type: restaurantsActionTyps.FETCH_SUCCESS,
+          payload: {
+            restaurants: data.restaurants
+          }
+        })
       )
   }, [])
   return (
@@ -40,6 +55,13 @@ export const Restaurants = () => {
       <MainCoverImageWrapper>
         <MainCover src={MainCoverImage} alt="main cover" />
       </MainCoverImageWrapper>
+      {
+        state.restaurantsList.map(restaurant =>
+          <div>
+            {restaurant.name}
+          </div>
+        )
+      }
     </Fragment>
   )
 }
